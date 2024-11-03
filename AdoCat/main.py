daeee6b93c5dbf55efda8a78d70dc4fbe515e7be
gKyoga <gus.vieira.pf@gmail.com>
2024-11-03 19:36:31 -0300

    manipulação de dados do banco no html

--- AdoCat/main.py
+++ AdoCat/main.py
@@ -1,32 +1,46 @@
 from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:toor@1234@localhost/db_adocat'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}{versao}'.format(
+        SGBD = 'mysql+pymysql',
+        senha = 'Kyoga%402003',
+        usuario = 'Kyoga',
+        servidor = '127.0.0.1',
+        database = 'db_adocat',
+        versao = '?charset=utf8mb4',
+    )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 class Usuario(db.Model):
     __tablename__ = 'tbl_users'
+    
     id_user = db.Column(db.Integer, primary_key=True)
     nome_user = db.Column(db.String(100), nullable=False)
     email_user = db.Column(db.String(100), unique=True, nullable=False)
     senha_user = db.Column(db.String(255), nullable=False)
-    data_criacao = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    data_criacao = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 class Gato(db.Model):
     __tablename__ = 'tbl_gatos'
+    
     id_gato = db.Column(db.Integer, primary_key=True)
     nome_gato = db.Column(db.String(100), nullable=False)
-    idade_gato = db.Column(db.Integer, nullable=False)
+    idade_gato = db.Column(db.String(15), nullable=False)
+    sexo_gato = db.Column(db.String(6), nullable=False)
+    vacina_gato = db.Column(db.Boolean, nullable=False, default=False)
     descricao_gato = db.Column(db.Text)
     foto_gato = db.Column(db.String(255))
     status_gato = db.Column(db.String(50), nullable=False, default='Disponível')
     user_id = db.Column(db.Integer, db.ForeignKey('tbl_users.id_user'))
-    data_cadastro = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    data_cadastro = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    usuario = db.relationship('Usuario', backref=db.backref('gatos', lazy=True))
 
 #importação das rotas para o progama principal
 from routes import *
--- AdoCat/routes.py
+++ AdoCat/routes.py
@@ -1,7 +1,11 @@
-from main import app, render_template
+from main import app, render_template,Gato
 
 
 #rota da pagina "home"
 @app.route('/')
 def home():
-    return render_template("homepage.html")
+    lista_gatos = Gato.query.order_by(Gato.id_gato)
+
+    return render_template("homepage.html",
+                           gatos_listados = lista_gatos,
+                           nomeGato = Gato.nome_gato)
